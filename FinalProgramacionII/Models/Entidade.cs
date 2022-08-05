40c11996439906883c9cfc67dbee2d5e2f071b6d
RosarioDaury <dauryjoserosariocaba@gmail.com>
2022-08-05 22:34:08 +0200

    MVC Set  up

---
+++ FinalProgramacionII/Models/Entidade.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProgramacionII.Models
+{
+    public partial class Entidade
+    {
+        [Required]
+        public int IdEntidades { get; set; }
+        [Required]
+        [StringLength(120)]
+        public string Descripcion { get; set; } = null!;
+        [Required]
+        public string Direccion { get; set; } = null!;
+        [Required]
+        public string Localidad { get; set; } = null!;
+        public string? TipoEntidad { get; set; }
+        public string? TipoDocumento { get; set; }
+        [Required]
+        public int NumeroDocumento { get; set; }
+        [Required]
+        public string Telefonos { get; set; } = null!;
+        public string? UrlpaginaWeb { get; set; }
+        public string? Urlfacebook { get; set; }
+        public string? Urlinstagram { get; set; }
+        public string? Urltwitter { get; set; }
+        public string? UrltikTok { get; set; }
+        public string? CodigoPostal { get; set; }
+        public string? CoordenadasGps { get; set; }
+        public int? LimiteCredito { get; set; }
+        [Required]
+        public string UserNameEntidad { get; set; } = null!;
+        [Required]
+        public string PasswordEntidad { get; set; } = null!;
+        public string? RolUserEntidad { get; set; }
+        public string? Comentario { get; set; }
+        public string? Status { get; set; }
+        public bool? NiEliminable { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+    }
+}
---
+++ FinalProgramacionII/Models/SellPointContext.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FinalProgramacionII.Models
+{
+    public partial class SellPointContext : DbContext
+    {
+        public SellPointContext()
+        {
+        }
+
+        public SellPointContext(DbContextOptions<SellPointContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Entidade> Entidades { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-MP2A3IP\\SQLEXPRESS;Database=SellPoint;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entidade>(entity =>
+            {
+                entity.HasKey(e => e.IdEntidades)
+                    .HasName("PK__Entidade__E0C75AC7AC508131");
+
+                entity.HasIndex(e => new { e.Descripcion, e.TipoEntidad, e.TipoDocumento, e.NumeroDocumento }, "EntidadesIndex");
+
+                entity.Property(e => e.CodigoPostal)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Comentario).HasColumnType("text");
+
+                entity.Property(e => e.CoordenadasGps)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("CoordenadasGPS");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(120)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Direccion).HasColumnType("text");
+
+                entity.Property(e => e.FechaRegistro).HasColumnType("date");
+
+                entity.Property(e => e.Localidad)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PasswordEntidad)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.RolUserEntidad)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("('User')");
+
+                entity.Property(e => e.Status)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("Status_")
+                    .HasDefaultValueSql("('Activa')");
+
+                entity.Property(e => e.Telefonos)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TipoDocumento)
+                    .HasMaxLength(9)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("('RNC')");
+
+                entity.Property(e => e.TipoEntidad)
+                    .HasMaxLength(8)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("('Juridica')");
+
+                entity.Property(e => e.Urlfacebook)
+                    .HasMaxLength(120)
+                    .IsUnicode(false)
+                    .HasColumnName("URLFacebook");
+
+                entity.Property(e => e.Urlinstagram)
+                    .HasMaxLength(120)
+                    .IsUnicode(false)
+                    .HasColumnName("URLInstagram");
+
+                entity.Property(e => e.UrlpaginaWeb)
+                    .HasMaxLength(120)
+                    .IsUnicode(false)
+                    .HasColumnName("URLPaginaWeb");
+
+                entity.Property(e => e.UrltikTok)
+                    .HasMaxLength(120)
+                    .IsUnicode(false)
+                    .HasColumnName("URLTikTok");
+
+                entity.Property(e => e.Urltwitter)
+                    .HasMaxLength(120)
+                    .IsUnicode(false)
+                    .HasColumnName("URLTwitter");
+
+                entity.Property(e => e.UserNameEntidad)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
