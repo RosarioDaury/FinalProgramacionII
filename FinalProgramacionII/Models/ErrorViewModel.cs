ea9ae648026b5c48e2a3a55b97966ab0cdcad84e
RosarioDaury <dauryjoserosariocaba@gmail.com>
2022-08-05 19:50:25 +0200

    Add project files.

---
+++ FinalProgramacionII/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalProgramacionII.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
